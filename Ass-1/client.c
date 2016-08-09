73e22ef18abd410b006d9b80a8ed143cdbad0d1b
Akhil <akhilvemula95@gmail.com>
2016-08-09 18:52:08 +0530

    Final submission

--- Ass-1/client.c
+++ Ass-1/client.c
@@ -1,100 +1,112 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <netdb.h>
 
 int main(int argc, char *argv[]) {
-   int sockfd, portno, n;
-   struct sockaddr_in serv_addr;
-   struct hostent *server;
-   
+   //Intialize the buffer to hold all fread and fwrite strings
    char* buffer = (char*)malloc(4096*(sizeof(char)));
- 
-   
-   if (argc < 3) {
-      fprintf(stderr,"usage %s hostname port\n", argv[0]);
-      exit(0);
-   }
-   
-   portno = atoi(argv[2]);
-   
-   /* Create a socket point */
-   sockfd = socket(AF_INET, SOCK_STREAM, 0);
-   
-   if (sockfd < 0) {
-      perror("ERROR opening socket");
+   //this is the port number obtained from argument   
+   int port = atoi(argv[2]);
+   //opens the socket here
+   int sock_desc = socket(AF_INET, SOCK_STREAM, 0);   
+   if (sock_desc < 0) {
+      fputs("Cannot connect to socket", stderr);
       exit(1);
    }
-   
-   server = gethostbyname(argv[1]);
-   
+   // Ip-address of the server
+   struct hostent* server = gethostbyname(argv[1]);
+   //If IP-Address not found
    if (server == NULL) {
-      fprintf(stderr,"ERROR, no such host\n");
-      exit(0);
+      fputs("no such Ip-address found", stderr);
+      return;
    }
-   
-   bzero((char *) &serv_addr, sizeof(serv_addr));
-   serv_addr.sin_family = AF_INET;
-   bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-   serv_addr.sin_port = htons(portno);
+   //This data structure stores the port and IP-Address of server
+   struct sockaddr_in addr;
+   memset((char *) &addr,0 ,sizeof(addr) );
+   //bzero((char *) &addr, sizeof(addr));
+   addr.sin_family = AF_INET;
+
+   bcopy((char *)server->h_addr, (char *)&addr.sin_addr.s_addr, server->h_length);
+   addr.sin_port = htons(port);
    
    /* Now connect to the server */
-  connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+  connect(sock_desc, (struct sockaddr*)&addr, sizeof(addr));
  
    
-/* read stream */
-   FILE* rx = fdopen(sockfd, "r");
+   /* read stream */
+   FILE* rx = fdopen(sock_desc, "r");
 
-/*write stream */
-   FILE* tx = fdopen(dup(sockfd), "w");
+   /*write stream */
+   FILE* tx = fdopen(dup(sock_desc), "w");
 
-
-   //memset(buffer , 0, 4095);
-   //bzero(buffer,4095);
-   n = fread(buffer,1,255,rx);
+   int number = fread(buffer,1,255,rx);
+   //Welcome message is being read
    printf("%s\n",buffer );
-   if (n < 0) {
-      perror("ERROR reading from socket");
-      exit(1);
-   }
+   if (number < 0) 
+      {
+         perror("ERROR reading from socket");
+         exit(1);
+      }
 
-
+//This while loop indicates that client can request any number of files. This will break only if client disconnects
 while(1)
 {
    printf("Please type the name of the file : ");
-   memset(buffer , 0, 4095);
-   fgets(buffer,4095,stdin);
-   
-   n = fwrite(buffer , 1 , 4095 , tx );
+   memset(buffer , 0, 4096);
+   //taking input from user regarding the file
+   fgets(buffer,4096,stdin);
+   //Sending the name of the file to server
+   number = fwrite(buffer , 1 , 4096 , tx );
    fflush(tx);
-   printf("The text from above file is:" );
-
-memset(buffer , '\0', 4095);
-
-while(1)
-{
-n = fread(buffer,1,4095,rx);
-
-printf("%s",buffer );
-if(buffer[4090] == '\0' )
-{
-break;
-printf("\n");
-}
-
-}
-}
+   
+   memset(buffer , '\0', 4096);
+   //Reading the size of the file from Server
+   number = fread(buffer,1,10,rx);
+   int size  = atoi(buffer);
+   int quotient = atoi(buffer)/4096;
+   int remainder = atoi(buffer)%4096;
+   memset(buffer , '\0', 4096);
+   //If file doesn't exist, then Display no file has been found
+   if(size == 0)
+      {
+      number = fread(buffer,1,4096,rx);
+      printf("%s\n",buffer );
+      }
+   //If file is there, take in buffer of 4096 bytes and keep printing 4096 bytes
+   else 
+      {
+      printf("The text from above file is:" );
+      int i = 0;
+      while(i < quotient)
+      {
+      number = fread(buffer,1,4096,rx);
+      //Keep printing buffer of 4096 bytes from a large file
+      printf("%s",buffer );
+      i = i +1;
+      }
+      //If any bytes remaing, print them too
+      if(remainder != 0)
+      {
+         number = fread(buffer,1,4096,rx);
+         printf("%s\n",buffer ); }
+      else
+         printf("\n");
+      }
+ }
 fclose(rx);
 fclose(tx);
    //bzero(buffer,4096);
    //scanf("%s",buffer);
 
    //printf("%s\n",buffer);
-   //stream = fdopen(sockfd, "r+b");
+   //stream = fdopen(sock_desc, "r+b");
    //n = fread(buffer,1,4096,stdin);
    //n = fwrite(buffer , 1 , 4095 , stream );
-   //n = write(sockfd,buffer,255);
+   //n = write(sock_desc,buffer,255);
 
 return 0;
 
--- Ass-1/server.c
+++ Ass-1/server.c
@@ -8,164 +8,140 @@
 
 int main(int argc, char* argv[])
 {
+  //First call to socket function
+  int sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+  struct sockaddr_in addr, client_addr;
+  //Setting all variables to NULL
+  memset(&client_addr , 0, sizeof(struct sockaddr));
+  //First argument is the port number. Here it is converted to Netwrok Byte Order
+  addr.sin_port = htons(atoi(argv[1]));
+  addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  addr.sin_family = AF_INET;
+  //If port not available inform the user about that
+  if(bind(sock_desc, (struct sockaddr*) &addr,sizeof(struct sockaddr)) < 0)
+    { 
+      fputs("Cannot bind to the Port !!", stderr);
+    }
 
-int sock_desc = socket(AF_INET, SOCK_STREAM, 0);
-struct sockaddr_in addr, client_addr;
-memset(&client_addr , 0, sizeof(struct sockaddr));
-addr.sin_port = htons(atoi(argv[1]));
-inet_pton(AF_INET,"127.0.0.1" , &addr.sin_addr );
-addr.sin_family = AF_INET;
-if ( bind(sock_desc, (struct sockaddr*) &addr,sizeof(struct sockaddr)) < 0 )
-	{	fputs("Cannot bind to the Port !!", stderr);
-	}
+  int list = listen(sock_desc,10);
+  if(list < 0 )
+    fputs("Cannot listen on the port!!", stderr);
 
-int list = listen(sock_desc,10);
-if(list < 0 )
-	fputs("Cannot listen on the port!!", stderr);
+  int addrlen = sizeof(struct sockaddr);
 
-int addrlen = sizeof(struct sockaddr);
+  //This loop suggests to continously wait for all clients
+  while(1)
+    {
+    int descrip = accept(sock_desc , (struct sockaddr*) &client_addr , &addrlen);
+    if(descrip == -1)
+      fputs("Descirptor not successfully returned!!", stderr);
 
-while(1)
-{
-int descrip = accept(sock_desc , (struct sockaddr*) &client_addr , &addrlen);
+    //Client is connected
+    printf("Client is connected\n");
+    //Buffer size of length 4096 for reading inputs from clients and sending message to clients
+    char* buf = (char*)malloc(4096*(sizeof(char)));
 
-if(descrip == -1)
-	fputs("Descirptor not successfully returned!!", stderr);
+    /* read stream */
+    FILE* rx = fdopen(descrip, "r");
+    /*write stream */
+    FILE* tx = fdopen(dup(descrip), "w");
 
-printf("Client is connected\n");
-char* buf = (char*)malloc(4096*(sizeof(char)));
-/* read stream */
-   FILE* rx = fdopen(descrip, "r");
+    //Sending welcome message to the client to indicate succesful connection of client
+    char wel_msg[] = "Welcome to the program, Sir!!\0";
 
-/*write stream */
-   FILE* tx = fdopen(dup(descrip), "w");
+    //number indicates the number of bytes written to the client
+    int number = fwrite(wel_msg, 1 , 255,tx );
+    fflush(tx);
+    if(number < 0)
+      {
+        fputs("Failed to write", stderr);    
+      }
+    //This loop indicates that server listens to a particular client unless it closes it's connection
+    while(1)
+      {
+      //This fread is for reading the file's name from the client
+      number = fread(buf, 1, 4096 , rx);
+      //If a client closes down it's connection, the loop breaks 
+      if(feof(rx))
+        {
+          printf("Client has disconnected\n");
+          break;
+        }
+      //i counts the number of characters in the name of the file name sent by client
+      int i;
+      for (i = 0; buf[i] != '\0'; i++)
+        {
+          
+        }
+      //f_name is the file's name as sent by client.
+      char* f_name = (char*)malloc(i*(sizeof(char)));
+      int j;i--;
+      for (j = 0; j < i; j++)
+        {
+          f_name[j] = buf[j];
+        }
+      //Since buf is no longer used, we initialize it to NULL
+      memset(buf, 0, sizeof(buf));
+      //Opens the file here
+      FILE* fp = fopen(f_name, "r");
+      //This is for knowing the size of the requested file and sending it to client
 
-//memset(buf , 0, 4095);
+      //If file does not exist, the following at the bottom happens
+      if (!fp) 
+        {
+          int size = 0;
+          char sizes[10];
+          sprintf(sizes, "%d", size);
+          fwrite(sizes,1,10,tx);
+          snprintf(buf, 4096, "Wrong file name!\n ");
 
-char wel_msg[] = "Welcome to the program, Sir!!\0";
-int number = fwrite(wel_msg, 1 , 255,tx );
-fflush(tx);
-if(number < 0)
-{
-  fputs("Failed to write", stderr);
-  
-}
-
-while(1)
-{
-
-number = fread(buf, 1, 4095 , rx);
-if(feof(rx))
-{
-  printf("Client has disconnected\n");
-  break;
-}
-int i;
-for (i = 0; buf[i] != '\0'; i++){
-  
-}
-char* f_name = (char*)malloc(i*(sizeof(char)));
-//char f_name[i];
-//memset(f_name, '\0', sizeof(f_name));
-
-int j;i--;
-for (j = 0; j < i; j++){
-  f_name[j] = buf[j];
-}
-
-
-
-
-//strcpy(f_name, buf);
-memset(buf, 0, sizeof(f_name));
-
-FILE* fp = fopen(f_name, "r");
-int c;
-i = 0;
-int done = 1;
-if (!fp) {
-    /* error message if appropriate */
-    //fputs("Wrong file name !!", stderr);
-  snprintf(buf, 4095, "Wrong file name!\n ");
-    done = 0;
-}
-if(done == 1)
-{
-while(1)
-{
-  if(i == 4094)
-  {
-    fwrite(buf,1,4095, tx);
-    memset(buf,0,4095);
-    i= 0;
-  }
-
-
-  c = fgetc(fp);
-  buf[i] = (char) c;
-  if( feof(fp) )
-  { 
-     break ;
-  }
-  //printf("%c", c);
-  i++;
-}
-buf[i] = '\0';
-}
-
-fwrite(buf,1,4095, tx);
-fflush(tx);
-//fclose(fp);
-
-
-
-}
-
-
-
-
-//printf("%s\n",buf);
-fclose(rx);
-fclose(tx);
-
-}
-//int n = write(descrip,"I got your message",5);
-/*FILE * stream = fdopen(descrip, "r+b");
-char* buf = (char*)malloc(4096*(sizeof(char)));
-memset(buf , 0, 4096);
-buf = "Welcome to the program, Sir!!\n";
-int number = fwrite(buf , 1 , strlen(buf) , stream );
-if(number < 0)
-	fputs("Failed to write", stderr);*/
-	
-
-//fflush(stream);
-
-//rewind(stream);
-//stream = fdopen(descrip, "r+b");
-//number = fread(buf,1,4096,stream);
-// number = read(descrip,buf, 255);
-//printf("%s\n",buf );
-
-/*memset(&buf , 0, 4096);
-number = fread(buf, 1, 30 , stream);
-if(number < 0) 
-	fputs("Failed to read", stderr);
-printf("")*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-return 0;
-
+          fwrite(buf,1,4096, tx);
+        }
+      //if file opens succesfully, all the text sending part to client happens here
+      else 
+        {
+          fseek(fp, 0L, SEEK_END);
+          //This is done to know the size of the file
+          int size = ftell(fp);
+          char sizes[10];
+          sprintf(sizes, "%d", size);
+          //telling the client the size of the file
+          fwrite(sizes,1,10,tx);
+          //Resetting the file pointer
+          rewind(fp);
+          int c;
+          i = 0;
+          int done = 1;
+          //This while loop runs till it reaches the end of file
+          while(1)
+            {
+              if(i == 4096)
+                {
+                  fwrite(buf,1,4096,tx);
+                  memset(buf,0,4096);
+                  i= 0;
+                }
+              c = fgetc(fp);
+              buf[i] = (char) c;
+              //The while loop breaks out here if end of file is reached
+              if(feof(fp))
+                { 
+                   break ;
+                }
+              i++;
+            }
+          //If the file is not exatly of size 4096 bytes
+          if(i!=0)
+            {
+              buf[i] = '\0';
+              fwrite(buf,1,4096,tx);
+            }
+          }
+      
+      fflush(tx);
+      }
+    fclose(rx);
+    fclose(tx);
+    }
+  return 0;
 }
